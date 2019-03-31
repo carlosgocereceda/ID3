257d595865a73e22f8cf2f775ab003936322a9d0
carlosgocereceda <carlosgcereceda@hotmail.es>
2019-03-31 16:47:13 +0200

    Primera vista con Jung

--- src/presentacion/Main.java
+++ src/presentacion/Main.java
@@ -17,6 +17,13 @@ public class Main {
 			ID3 id3 = new ID3();
 			id3.algoritmo(l.getTabla(), null);
 			ArrayList<Nodo> arbol = id3.getArbol();
+			for(int i = 0; i < arbol.size(); i++) {
+				String padre = "";
+				if(arbol.get(i).getPadre() != null)
+					padre = arbol.get(i).getPadre().getAtributo() + "->" + arbol.get(i).getPadre().getCamino();
+				System.out.println(arbol.get(i).getCamino() + " " + arbol.get(i).getAtributo() + " padre: " + padre);
+			}
+			//Vista vista = new Vista(arbol);
 			System.out.println("PAUSA");
 		} catch (IOException e) {
 			e.printStackTrace();
